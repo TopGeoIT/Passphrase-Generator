c5b162450a80b81eadc67c1b5ce54725be86f0e4
gumkac <geogumkac@gmail.com>
2016-03-31 13:54:39 +0200

    bugfixing webservice, add android app project into solution

---
+++ Web/Controllers/PassphraseGenController.cs
@@ -0,0 +1,67 @@
+﻿using PassphraseGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web.Controllers
+{
+    public class PassphraseGenController : ApiController
+    {
+
+        
+
+        public string Get([FromUri]int bits)
+        {
+            try {
+                PassphraseController passphrasegen = new PassphraseController(AppDomain.CurrentDomain.BaseDirectory + "/dicts/"/*"c:/skola/8.semester/TP/Generator/Dictionaries/"*/);
+                string[] veta = passphrasegen.generateSentenceFromEntrophy(bits);
+                string celaveta = "";
+                for (int i = 0; i < veta.Length; i++)
+                {
+                    celaveta += veta[i] + " ";
+                }
+                return celaveta;
+            }catch(Exception e)
+            {
+                return e.Message + " " + e.InnerException;
+            }
+        }
+
+        public string Post([FromBody]string value)
+        {
+            try
+            {
+                PassphraseController passphrasegen = new PassphraseController(AppDomain.CurrentDomain.BaseDirectory  + "/dicts/"/*"c:/skola/8.semester/TP/Generator/Dictionaries/"*/);
+                string[] veta = passphrasegen.generateSentenceFromBinary(value);
+                string celaveta = "";
+                for (int i = 0; i < veta.Length; i++)
+                {
+                    celaveta += veta[i] + " ";
+                }
+                return celaveta;
+            }
+            catch (Exception e)
+            {
+                return e.Message + " " + e.InnerException;
+            }
+        }
+
+        public string Put([FromBody]string value)
+        {
+            try
+            {
+                PassphraseController passphrasegen = new PassphraseController(AppDomain.CurrentDomain.BaseDirectory  + "/dicts/"/*"c:/skola/8.semester/TP/Generator/Dictionaries/"*/);
+                string binary = passphrasegen.generateBinaryFromSentence(value);
+                return binary;
+            }
+            catch (Exception e)
+            {
+                return e.Message + " " + e.InnerException;
+            }
+        }
+
+    }
+}
---
+++ passphrase/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace passphrase
+{
+	[Activity (Label = "passphrase", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.Main);
+		}
+
+		public override bool OnCreateOptionsMenu(IMenu menu)
+		{
+			MenuInflater.Inflate(Resource.Menu.MainMenu, menu);
+			return true;
+		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			switch (item.ItemId)
+			{
+			case Resource.Id.generateFromEntropy: 
+				StartActivity (typeof(GenerateEntropyActivity));
+				return true;
+			case Resource.Id.translator:
+				StartActivity(typeof(TranslateActivity));
+				return true;
+			case Resource.Id.closeApp:
+				System.Environment.Exit(0);
+				return true;
+			default:
+				return false;
+			}
+		}
+	}
+}
